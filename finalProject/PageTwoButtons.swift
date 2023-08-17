6f3800f1e988102560aab8e768750ff872cfcc57
Manvi <manvib7423@gmail.com>
2023-08-17 13:24:16 -0400

    Buttons page 2

---
+++ finalProject/PageTwoButtons.swift
@@ -0,0 +1,18 @@
+//
+//  PageTwoButtons.swift
+//  finalProject
+//
+//  Created by Manvi Boppana on 8/17/23.
+//
+
+import SwiftUI
+
+struct PageTwoButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageTwoButtons()
+}
