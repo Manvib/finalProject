b68d0333af8c5058b312872ae0fa2d3f118dc511
Manvi <manvib7423@gmail.com>
2023-08-17 13:25:01 -0400

    buttons page 2

--- finalProject/PageTwoButtons.swift
+++ finalProject/PageTwoButtons.swift
@@ -9,8 +9,163 @@ import SwiftUI
 
 struct PageTwoButtons: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            Text("What are you discarding?")
+                .font(.headline)
+                
+           
+            HStack(spacing: 20){
+                Button {
+                    
+                } label: {
+                    VStack(alignment: .center) {
+                        Image(systemName: "wineglass")
+                        Text("Glass Containers")
+                    }
+                    
+                    
+                    
+                }
+                
+                .font(.headline.bold())
+                .foregroundColor(.white)
+                .frame(height: 100)
+                .frame(maxWidth: .infinity)
+                .background(Color.green)
+                .cornerRadius(8)
+                
+                Button {
+                    
+                } label: {
+                    VStack {
+                        Image(systemName: "fork.knife")
+                        Text("Plastic")
+                    }
+                }
+                .font(.headline.bold())
+                .foregroundColor(.white)
+                .frame(height: 100)
+                .frame(maxWidth: .infinity)
+                .background(Color.green)
+                .cornerRadius(8)
+            } .padding(.horizontal, 20)
+            
+            HStack(spacing: 20){
+                Button {
+                } label: {
+                               VStack(alignment: .center) {
+                                   Image(systemName: "batteryblock")
+                                   Text("Batteries")
+                               }
+               
+               
+               
+                           }
+               
+                           .font(.headline.bold())
+                           .foregroundColor(.white)
+                           .frame(height: 100)
+                           .frame(maxWidth: .infinity)
+                           .background(Color.green)
+                           .cornerRadius(8)
+               
+                           Button {
+               
+                           } label: {
+                               VStack {
+                                   Image(systemName: "tshirt")
+                                   Text("Clothes")
+                               }
+                           }
+                           .font(.headline.bold())
+                           .foregroundColor(.white)
+                           .frame(height: 100)
+                           .frame(maxWidth: .infinity)
+                           .background(Color.green)
+                           .cornerRadius(8)
+                
+               
+            }  .padding(.horizontal, 20)
+            
+            HStack(spacing: 20){
+                Button {
+              
+                          } label: {
+                              VStack(alignment: .center) {
+                                  Image(systemName: "carrot")
+                                  Text("Veggie Scraps")
+                              }
+              
+              
+              
+                          }
+              
+                          .font(.headline.bold())
+                          .foregroundColor(.white)
+                          .frame(height: 100)
+                          .frame(maxWidth: .infinity)
+                          .background(Color.green)
+                          .cornerRadius(8)
+              
+                          Button {
+              
+                          } label: {
+                              VStack {
+                                  Image(systemName: "shippingbox")
+                                  Text("Cardboard")
+                              }
+                          }
+                          .font(.headline.bold())
+                          .foregroundColor(.white)
+                          .frame(height: 100)
+                          .frame(maxWidth: .infinity)
+                          .background(Color.green)
+                          .cornerRadius(8)
+              
+            }  .padding(.horizontal, 20)
+            
+            
+            HStack(spacing: 20){
+                Button {
+              
+                          } label: {
+                              VStack(alignment: .center) {
+                                  Image(systemName: "leaf")
+                                  Text("Yard Waste")
+                              }
+              
+              
+                          }
+              
+                          .font(.headline.bold())
+                          .foregroundColor(.white)
+                          .frame(height: 100)
+                          .frame(maxWidth: .infinity)
+                          .background(Color.green)
+                          .cornerRadius(8)
+              
+                Button {
+              
+                          } label: {
+                              VStack {
+                                  Image(systemName: "takeoutbag.and.cup.and.straw")
+                                  Text("Used Food Boxes")
+                              }
+                          }
+                          .font(.headline.bold())
+                          .foregroundColor(.white)
+                          .frame(height: 100)
+                          .frame(maxWidth: .infinity)
+                          .background(Color.green)
+                          .cornerRadius(8)
+              //
+            }  .padding(.horizontal, 20)
+         
+            //
+        }
     }
+
 }
 
 #Preview {
